0a6c7b9a2981c98c1a334f37046d98ab6b049684
st141283 <krivcenkodaorsk567@gmail.com>
2025-10-21 12:51:57 +0300

    feat: command line

--- sum_prod.cpp
+++ sum_prod.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
 
-int main(){
-	unsigned long long x;
-	std::cout << "Введите число ";
-	std::cin >> x;
-
+int main(int argc, char* argv[]){
 	unsigned long long sum = 0, prod = 1;
-	do{
-		sum += x%10;
-		prod *= x%10;
-		x /= 10;
-	}while (x != 0);
+	if (argc > 1){
+		char *number = argv[1];
+		int index = 0;
+		while (number[index] != '\0' && index  < 11){
+			if (number[index] >= '0' && number[index]<='9'){
+				sum += (number[index]-'0');
+				prod *= (number[index]-'0');
+			}else {
+				std::cout << "Wrong number" << std::endl;
+				return 1;
+			}
+			++index;
+		}
+		std::cout << sum << " " << prod << std::endl;
+	}else{
 
-	std::cout << sum << " " << prod << std::endl;
+		unsigned long long x;
+		std::cout << "Введите число ";
+		std::cin >> x;
+
+		do{
+			sum += x%10;
+			prod *= x%10;
+			x /= 10;
+		}while (x != 0);
+
+		std::cout << sum << " " << prod << std::endl;
+	}
 
 	return 0;
 }
